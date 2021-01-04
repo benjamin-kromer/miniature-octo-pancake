21359380cd21f1b97111c698cc7ebb52d97eb2dc
benjamin-kromer <benjamin.kromer1986@gmail.com>
2021-01-05 00:41:24 +0100

    implemented cookies with passport

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 //==========================================
 
 const app = express();
@@ -14,6 +15,14 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+//==========================================
+app.use(session({
+    secret: process.env.COOKIE_SECRET,
+    resave: false,
+    saveUninitialized:false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 //==========================================
 
 mongoose.connect('mongodb://localhost:27017/'+process.env.DB_NAME,{useNewUrlParser: true, useUnifiedTopology: true}); //?retryWrites=true&w=majority
@@ -29,8 +38,13 @@ const userSchema = new mongoose.Schema({
     }
 });
 
+userSchema.plugin(passportLocalMongoose);
+
 const User = new mongoose.model('User',userSchema);
 
+passport.use(User.createStrategy());
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 //==========================================
 app.get('/',(req,res)=>{
     res.render('home',{})
@@ -42,55 +56,13 @@ app.get('/register',(req,res)=>{
     res.render('register',{})
 });
 app.post('/register',(req,res)=>{
-    //console.log("req.body: ",req.body);
-    bcrypt.hash(req.body.password,saltRounds,(err,hash)=>
-    {
-        if(!err){
-            User.find({email: req.body.email},(err,foundUser)=>{
-            if(!err){
-                //console.log(foundUser);
-                //console.log(foundUser.length);
-                if(foundUser.length === 0){
-                    const newUser = new User({
-                        email: req.body.email,
-                        password: hash
-                    });
-                    newUser.save();
-                    console.log(`User registered!` );
-                    res.render('login');
-                }else{
-                    //console.log(`User already exists ${foundUser}!`);
-                    res.redirect('login');
-                }
-            }else{
-                console.log(err);
-                res.send(err);
-            }
-        })}else{
-            console.log(err);
-            res.send(err);
-        }
-    })
+    
+    
 });
 app.post('/login',(req,res)=>{
-    console.log(req.body);
-    const email = req.body.email;
-    const password = req.body.password;
-   
-    User.findOne({email:email},(err,foundUser)=>{
-        if(!err){
-             bcrypt.compare(req.body.password,foundUser.password,(err,result)=>{
-                if(result === true){
-                    res.render('secrets');
-                }else{
-                    res.redirect('register');
-                }}) 
-        }else{
-            console.log("error",err);
-            res.send(err);
-            }}) 
-        });
-
+    
+    
+    });
 const listener = app.listen(process.env.PORT || 3000, function() {
     console.log("Server started on port "+listener.address().port);
   });
