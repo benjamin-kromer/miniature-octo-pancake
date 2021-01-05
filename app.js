b39e4b0a048eff364303d2309a11dbd47c8915bf
benjamin-kromer <benjamin.kromer1986@gmail.com>
2021-01-05 12:31:59 +0100

    added missing routes and fixed bad request err

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 //==========================================
 app.use(session({
-    secret: process.env.COOKIE_SECRET,
+    secret: "Our little secret.",//process.env.COOKIE_SECRET
     resave: false,
     saveUninitialized:false
 }));
@@ -29,14 +29,8 @@ mongoose.connect('mongodb://localhost:27017/'+process.env.DB_NAME,{useNewUrlPars
 //"mongodb+srv://"+process.env.DB_USERNAME+":"+process.env.DB_PASSWORD+process.env.DB_CLUSTER+"/"+process.env.DB_NAME
 mongoose.set('useCreateIndex', true)
 const userSchema = new mongoose.Schema({
-    email: {
-        type:String,
-        required: true
-    },
-    password: {
-        type:String,
-        required: true
-    }
+    email:String,
+    password: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -44,32 +38,47 @@ userSchema.plugin(passportLocalMongoose);
 const User = new mongoose.model('User',userSchema);
 
 passport.use(User.createStrategy());
-passport.serializeUser(User.serializeUser());
-passport.deserializeUser(User.deserializeUser());
+/* passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser()); */
+
+
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+  });
+  
+  passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+      done(err, user);
+    });
+  });
 //==========================================
 app.get('/',(req,res)=>{
-    res.render('home',{})
+    res.render('home')
 });
 app.get('/login',(req,res)=>{
-    res.render('login',{})
+    res.render('login')
 });
 app.get('/register',(req,res)=>{
-    res.render('register',{})
+    res.render('register')
 });
-app.get('/secrets',(req,res)=>{
+app.get('/secrets',function (req,res){
     if(req.isAuthenticated()){
         res.render('secrets');
     }else{
         res.redirect('/login');
     }
 });
+app.get('/logout',(req,res)=>{
+    req.logout();
+    res.redirect('/');
+})
 app.post('/register',(req,res)=>{
-    User.register({email:req.body.email},req.body.password,(err,user)=>{
+    User.register({username: req.body.username},req.body.password,function(err,user){
         if(err){
             console.log(err);
             res.redirect('/register');
         }else{
-            passport.authenticate('local')(req,res,()=>{
+            passport.authenticate('local')(req,res,function(){
                 res.redirect('/secrets');
             })
         }    
@@ -77,10 +86,19 @@ app.post('/register',(req,res)=>{
     });
 app.post('/login',(req,res)=>{
     const user = new User({
-        email: req.body.email,
+        username: req.body.username,
         password: req.body.password
+    });
+    req.login(user,function(err){
+        if(err){
+            console.log(err);
+        }else{
+            passport.authenticate("local")(req,res,function(){
+                res.redirect("/secrets")
+            });
+        }
+       
     })
-    
     });
 const listener = app.listen(process.env.PORT || 3000, function() {
     console.log("Server started on port "+listener.address().port);
